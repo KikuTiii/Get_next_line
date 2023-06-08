3d452f4b1f447f70dbd53a433ccee928e0e837ba
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-08 18:33:57 -0300

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,82 +1,81 @@
 #include "get_next_line.h"
 
-char *lst_find(t_LIST *lst, char number)
+char	*lst_find(t_list *lst, char number)
 {
-    size_t index;
+	size_t	index;
 
-    index = 0;
-    while (lst->len_buffer > index)
-    {
-        if (lst->buffer[index] == number)
-            return (&lst->buffer[index]);
-        index++;
-    }
-    return (NULL);
+	index = 0;
+	while (lst->len_b > index)
+	{
+		if (lst->buffer[index] == number)
+			return (&lst->buffer[index]);
+		index++;
+	}
+	return (NULL);
 }
 
-int lst_expand(t_LIST *lst)
+int	lst_expand(t_list *lst)
 {
-    char *expand;
-    size_t index;
+	char	*expand;
+	size_t	index;
 
-    index = 0;
-    expand = malloc(sizeof(char) * (lst->cap_buffer * 2));
-    if (expand == NULL)
-        return (-1);
-    lst->cap_buffer = lst->cap_buffer * 2;
-    while (lst->len_buffer > index)
-    {
-        expand[index] = lst->buffer[index];
-        index++;
-    }
-    free(lst->buffer);
-    lst->buffer = expand;
-    return (0);
+	index = 0;
+	expand = malloc(sizeof(char) * (lst->cap_b * 2));
+	if (expand == NULL)
+		return (-1);
+	lst->cap_b = lst->cap_b * 2;
+	while (lst->len_b > index)
+	{
+		expand[index] = lst->buffer[index];
+		index++;
+	}
+	free(lst->buffer);
+	lst->buffer = expand;
+	return (0);
 }
 
-char *lst_getslice(t_LIST *lst, size_t start, size_t end)
+char	*lst_getslice(t_list *lst, size_t start, size_t end)
 {
-    char *slice;
-    size_t index;
+	char	*slice;
+	size_t	index;
 
-    index = start;
-    slice = malloc(sizeof(char) * (end - start) + 1);
-    if (slice == NULL)
-        return (NULL);
-    while (index < end)
-    {
-        slice[index] = lst->buffer[index];
-        index++;
-    }
-    slice[index] = '\0';
-    return (slice);
+	index = start;
+	slice = malloc(sizeof(char) * (end - start) + 1);
+	if (slice == NULL)
+		return (NULL);
+	while (index < end)
+	{
+		slice[index] = lst->buffer[index];
+		index++;
+	}
+	slice[index] = '\0';
+	return (slice);
 }
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-    char *dest1;
-    const char *dest2;
-    size_t index;
+	char		*dest1;
+	const char	*dest2;
+	size_t		index;
 
-    index = 0;
-    dest1 = (char *)dest;
-    dest2 = (const char *)src;
-    if (dest == 0 && src == 0)
-        return (dest);
-    if (dest1 > dest2)
-    {
-        while (n--)
-        {
-            dest1[n] = dest2[n];
-        }
-    }
-    else
-    {
-        while (index < n)
-        {
-            dest1[index] = dest2[index];
-            index++;
-        }
-    }
-    return (dest);
-}
+	index = 0;
+	dest1 = (char *)dest;
+	dest2 = (const char *)src;
+	if (dest == 0 && src == 0)
+		return (dest);
+	if (dest1 > dest2)
+	{
+		while (n--)
+		{
+			dest1[n] = dest2[n];
+		}
+	}
+	else
+	{
+		while (index < n)
+		{
+			dest1[index] = dest2[index];
+			index++;
+		}
+	}
+	return (dest);
