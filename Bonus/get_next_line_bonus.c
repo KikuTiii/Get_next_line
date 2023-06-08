3ea2699e4f3bc4ddaa959da68561020e7d042e31
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-08 18:35:14 -0300

    Update get_next_line_bonus.c

--- Bonus/get_next_line_bonus.c
+++ Bonus/get_next_line_bonus.c
@@ -1,48 +1,53 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-char *memslice(char *newline, t_LIST *lst, char *line)
+static char	*memslice(char	*newline, t_list *lst, char *line)
 {
-	line = lst_getslice(lst, 0, newline - lst->buffer);
+	line = lst_getslice(lst, 0, newline - lst->buffer + 1);
 	if (line == NULL)
 		return (NULL);
-	lst->len_buffer -= (newline + 1) - lst->buffer;
-	ft_memmove(lst->buffer, newline + 1, lst->len_buffer);
+	lst->len_b -= (newline + 1) - lst->buffer;
+	ft_memmove(lst->buffer, newline + 1, lst->len_b);
 	return (line);
 }
 
-char *lastmove(t_LIST *lst, char *line)
+static char	*lastslice(t_list *lst, char *line)
 {
-	if (lst->len_buffer > 0)
+	if (lst->len_b > 0)
 	{
-		line = lst_getslice(lst, 0, lst->len_buffer);
+		line = lst_getslice(lst, 0, lst->len_b);
 		if (line == NULL)
 			return (NULL);
 		free(lst->buffer);
 		lst->buffer = NULL;
-		lst->len_buffer = 0;
-		lst = 0;
+		lst->len_b = 0;
+		lst = NULL;
 		return (line);
 	}
-	free(lst->buffer);
+	if (lst->buffer[0] == '\0')
+	{
+		free(lst->buffer);
+		lst->buffer = NULL;
+	}
 	return (NULL);
 }
 
-char *lst_create(t_LIST *lst)
+static char	*lst_create(t_list *lst)
 {
 	if (lst->buffer == NULL)
 	{
 		lst->buffer = malloc(sizeof(char) * BUFFER_SIZE);
-		lst->cap_buffer = BUFFER_SIZE;
+		lst->buffer[0] = '\0';
+		lst->cap_b = BUFFER_SIZE;
 	}
 	return (lst->buffer);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static t_LIST fileds[MAX_FD];
-	char *newline;
-	t_LIST *lst;
-	int bytes;
+	static t_list		fileds[MAX_FD];
+	char				*newline;
+	t_list				*lst;
+	int					bytes;
 
 	if (fd < 0 || fd >= MAX_FD)
 		return (NULL);
@@ -53,14 +58,13 @@ char *get_next_line(int fd)
 		newline = lst_find(lst, '\n');
 		if (newline != NULL)
 			return (memslice(newline, lst, NULL));
-		if (lst->len_buffer >= lst->cap_buffer && lst->buffer != NULL)
+		if ((lst->cap_b - lst->len_b < BUFFER_SIZE) && lst->buffer != NULL)
 			if (lst_expand(lst) == -1)
 				return (NULL);
-		bytes = read(fd, lst->buffer + lst->len_buffer,
-					 lst->cap_buffer - lst->len_buffer);
-		if (bytes == 0)
-			return (lastmove(lst, NULL));
-		lst->len_buffer += bytes;
+		bytes = read(fd, lst->buffer + lst->len_b, BUFFER_SIZE);
+		if (bytes == -1 || bytes == 0)
+			return (lastslice(lst, NULL));
+		lst->len_b += bytes;
 	}
 	return (NULL);
 }
