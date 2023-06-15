c360e6870ff24563e7e0dd31ef91d8028bf558f5
Matheus Kikuti <111128991+KikuTiii@users.noreply.github.com>
2023-06-15 15:53:13 -0300

    Update get_next_line_bonus.c

--- Bonus/get_next_line_bonus.c
+++ Bonus/get_next_line_bonus.c
@@ -1,16 +1,16 @@
 #include "get_next_line_bonus.h"
 
-static char	*memslice(char	*newline, t_list *lst, char *line)
+char *memslice(char *newline, t_LIST *lst, char *line)
 {
 	line = lst_getslice(lst, 0, newline - lst->buffer + 1);
 	if (line == NULL)
-		return (NULL);
+		return NULL;
 	lst->len_b -= (newline + 1) - lst->buffer;
 	ft_memmove(lst->buffer, newline + 1, lst->len_b);
 	return (line);
 }
 
-static char	*lastslice(t_list *lst, char *line)
+char *lastmove(t_LIST *lst, char *line)
 {
 	if (lst->len_b > 0)
 	{
@@ -31,7 +31,7 @@ static char	*lastslice(t_list *lst, char *line)
 	return (NULL);
 }
 
-static char	*lst_create(t_list *lst)
+char *lst_create(t_LIST *lst)
 {
 	if (lst->buffer == NULL)
 	{
@@ -44,9 +44,9 @@ static char	*lst_create(t_list *lst)
 
 char	*get_next_line(int fd)
 {
-	static t_list		fileds[MAX_FD];
+	static t_LIST		fileds[MAX_FD];
 	char				*newline;
-	t_list				*lst;
+	t_LIST				*lst;
 	int					bytes;
 
 	if (fd < 0 || fd >= MAX_FD)
@@ -58,13 +58,50 @@ char	*get_next_line(int fd)
 		newline = lst_find(lst, '\n');
 		if (newline != NULL)
 			return (memslice(newline, lst, NULL));
-		if ((lst->cap_b - lst->len_b < BUFFER_SIZE) && lst->buffer != NULL)
+		if (lst->len_b >= lst->cap_b && lst->buffer != NULL)
 			if (lst_expand(lst) == -1)
 				return (NULL);
-		bytes = read(fd, lst->buffer + lst->len_b, BUFFER_SIZE);
-		if (bytes == -1 || bytes == 0)
-			return (lastslice(lst, NULL));
+		bytes = read(fd, lst->buffer + lst->len_b, lst->cap_b - lst->len_b);
+		if (bytes == -1)
+			return (NULL);
+		if (bytes == 0)
+			return (lastmove(lst, NULL));
 		lst->len_b += bytes;
 	}
-	return (NULL);
+	return NULL;
 }
+
+// int    main(void)
+// {
+//     int        fd;
+//     int        fd2;
+//     int        fd3;
+//     char    *line;
+//     int        index;
+
+//     index = 0;
+//     fd = open("Texto1.txt", O_RDONLY);
+//     fd2 = open("Texto2.txt", O_RDONLY);
+//     fd3 = open("Texto3.txt", O_RDONLY);
+//     while (index < 4)
+//     {
+//         line = get_next_line(fd);
+//         printf("%s\n", line);
+//         free(line);
+//         index++;
+//     }
+//     close(fd);
+//     while ((line = get_next_line(fd2)) != NULL)
+//     {
+//         printf("%s\n", line);
+//         free(line);
+//     }
+//     close(fd2);
+//     while ((line = get_next_line(fd3)) != NULL)
+//     {
+//         printf("%s\n", line);
+//         free(line);
+//     }
+//     close(fd3);
+//     return (0);
+// }
